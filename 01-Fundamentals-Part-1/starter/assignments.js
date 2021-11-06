09a3dea3b5016034dc3e01f533f783503c91791f
Faraz Chand <faraz.chand@hotmail.com>
2021-11-06 05:21:28 -0400

    Day 29
    - 2 hour session
    
    - DAY 1 of the JavaScript Course, day 29 of my webDev journey
    
    - spent about an hour in the morning on this course and an hour when I came back from work. Spent another 2 hours practicing CSS but I'm not including those in todays total
    
    - Finished the setup steps for this course
    
    - Learned about Linking JS files to your HTML, Values and Variables and Data types as well as completed 2 of the practice assignments
    
    - A lot of this content in the beginning are things I'm already familiar with, but I think it's good to go over the basics and strengthen my fundamentals before moving on to the harder parts of JavaScript - so I'm not going to skip anything

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,17 @@
+// 1. Values and Variables
+let country = "Canada";
+let continent = "North America";
+let population = 38000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//2. Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
