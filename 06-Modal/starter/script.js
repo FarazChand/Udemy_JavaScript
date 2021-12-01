5c26718d0d3c55a0c53396a5a8466b55a676ee54
Faraz Chand <faraz.chand@hotmail.com>
2021-12-01 07:42:20 -0500

    Day 50
    - 2 1/2 hour session
    
    - Started and finished Project #2 of this section
    
    - Learned how to work with classes, ex. adding and removing classes, checking if a class is contained in an element
    
    - Learned about Keypress events and how to create an "Esc" Keypress event, passing the event listner's object to it's function parameter and using the properties of the object within the event handler.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden'); // dont select with dot
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+console.log(btnsOpenModal);
+
+// Open Modal Window
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//
+// Hiding Modal Window
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//Hide with Esc key
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
