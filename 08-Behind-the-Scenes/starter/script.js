76c116fd0f59cfc0c0daa30b3bae3f7af2e7fb6e
Faraz Chand <faraz.chand@hotmail.com>
2021-12-05 07:11:55 -0500

    Day 54
    - 5 Hour Session
    
    - Learned about Scope and the Scope Chain, how it's a one way street, only look up not down or sideways. Much more to scope as  well
    
    - Looked at how scoping works in practice
    
    - Learned about the Variable Environment and Hosting, as well ass the TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      let firstName = `Steven`;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      // console.log(add(2, 3));
+      output = `NEW OUTPUT`;
+    }
+
+    // console.log(add(2, 3)); // reference error in strict mode
+    // console.log(str); // reference error
+    console.log(millenial);
+    console.log(output);
+  }
+  // console.log(millenial); // reference error
+
+  printAge();
+  return age;
+}
+
+let firstName = 'Jonas';
+calcAge(1991);
+console.log(firstName);
+// console.log(age);  // reference error
+// printAge(); // reference error
