c913f97d5bdd617a92939b46fc9e0cbcbf05ba99
Faraz Chand <faraz.chand@hotmail.com>
2021-12-06 05:11:21 -0500

    Day 55
    - practiced the Hoisting and TDZ concepts in VS Code
    
    - Learned about the "This" Keyword and how it differs in use depending on how it is called

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,38 +1,87 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      let firstName = `Steven`;
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       let firstName = `Steven`;
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-      // console.log(add(2, 3));
-      output = `NEW OUTPUT`;
-    }
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       // console.log(add(2, 3));
+//       output = `NEW OUTPUT`;
+//     }
 
-    // console.log(add(2, 3)); // reference error in strict mode
-    // console.log(str); // reference error
-    console.log(millenial);
-    console.log(output);
-  }
-  // console.log(millenial); // reference error
+//     // console.log(add(2, 3)); // reference error in strict mode
+//     // console.log(str); // reference error
+//     console.log(millenial);
+//     console.log(output);
+//   }
+//   // console.log(millenial); // reference error
 
-  printAge();
-  return age;
+//   printAge();
+//   return age;
+// }
+
+// let firstName = 'Jonas';
+// calcAge(1991);
+// console.log(firstName);
+// // console.log(age);  // reference error
+// // printAge(); // reference error
+
+//Hoisting with variables
+console.log(me); // undefined, does not throw error - sneaky bug
+// console.log(job); // Throws error
+// console.log(year); // Throws error
+
+var me = `Jonas`;
+let job = `teacher`;
+const year = 1991;
+
+// Hoisting with Functions
+console.log(addDecl(2, 3)); //function declarations are hoisted correctly
+// console.log(addExpr(2, 3));  // Expression Throws error
+// console.log(addArrow(2, 3)); // Expression Throws error
+// console.log(addArrow(2, 3)); // Error: Undefined is not a function
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-let firstName = 'Jonas';
-calcAge(1991);
-console.log(firstName);
-// console.log(age);  // reference error
-// printAge(); // reference error
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+var addArrow2 = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart(); // Undefined, sneaky bug
+
+!numProducts ? deleteShoppingCart() : console.log(numProducts); // Undefined, sneaky bug
+
+var numProducts = 10;
+
+!numProducts ? deleteShoppingCart() : console.log(numProducts); // var finally defined as intended
+
+function deleteShoppingCart() {
+  console.log(`All products deleted!`);
+}
+
+var x = 1; // creates property on the global window object
+let y = 2; // does not create property on the global window object
+const z = 3; // does not create property on the global window object
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
