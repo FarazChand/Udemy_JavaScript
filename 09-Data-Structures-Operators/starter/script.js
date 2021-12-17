8bc1a8dbc65bed41460f55ff8ed1f6bd9e73ae66
Faraz Chand <faraz.chand@hotmail.com>
2021-12-17 05:32:22 -0500

    Day 63
    - Learned about Short Circuiting using the "&&" and "||" logical operators
    
    - Learned about using the Nullish Coalescing Operator (??) in situations where we might need to consider the values "0" and "" (empty string) as valid inputs and only consider undefined and null as invalid
    
    - Learned how to use Logical Assignment Operators to write even less code when creating default values with the OR/NC operator, or when using the AND operator to create an if statement

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -346,4 +346,78 @@ const restaurant = {
 // restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 // restaurant.orderPizza('mushrooms');
 
-// Short Circuiting (&& and ||)
+// // Short Circuiting (&& and ||)
+
+// // Use ANY data type, return ANY data type, short-circuiting
+
+// console.log(`----- OR -----`);
+
+// console.log(3 || 'Jonas');
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// console.log(undefined || 0 || '' || 'hello' || 23 || null);
+
+// restaurant.numGuests = 0; // bug, will skip because false (see end)
+// // restaurant.numGuests = 23;|
+
+// // normal ternary operator to set default value
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// // using the || operator to set default value
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log(`----- AND -----`);
+
+// console.log(0 && 'Jonas');
+// console.log(7 && 'Jonas');
+
+// console.log('Hello' && 23 && null && `Jonas`);
+
+// //normal if statement
+// // if (restaurant.orderPizza) {
+// //   restaurant.orderPizza('mushrooms', 'spinach');
+// // }
+
+// // restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+// //end
+// // using Nullish Coalescing operator to allow 0 to work
+// //Nullish: null and undefined (Not 0 or '');
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
+
+// Logical Assignment Operators
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+//OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;   // 0 will short circuit
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10; // 0 will not short circuit, just undefined and null
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
