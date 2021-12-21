a2b4796e4873f830d9935236cea6ced6ea507a9a
Faraz Chand <faraz.chand@hotmail.com>
2021-12-21 06:35:10 -0500

    Day 63 - 66
    - 3 hours total
    
    - Have been slacking in my learning and git updates, need to get back on track
    
    - Did the coding challenge 1 of the section
    
    - Learned about the "for-of" loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -346,6 +346,8 @@ const restaurant = {
 // restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 // restaurant.orderPizza('mushrooms');
 
+//////////////////////////////////////////
+
 // // Short Circuiting (&& and ||)
 
 // // Use ANY data type, return ANY data type, short-circuiting
@@ -390,34 +392,144 @@ const restaurant = {
 // const guestCorrect = restaurant.numGuests ?? 10;
 // console.log(guestCorrect);
 
-// Logical Assignment Operators
+// // Logical Assignment Operators
 
-const rest1 = {
-  name: 'Capri',
-  // numGuests: 20,
-  numGuests: 0,
-};
+// const rest1 = {
+//   name: 'Capri',
+//   // numGuests: 20,
+//   numGuests: 0,
+// };
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
+// const rest2 = {
+//   name: 'La Piazza',
+//   owner: 'Giovanni Rossi',
+// };
 
-//OR assignment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-// rest1.numGuests ||= 10;   // 0 will short circuit
-// rest2.numGuests ||= 10;
+// //OR assignment operator
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+// // rest1.numGuests ||= 10;   // 0 will short circuit
+// // rest2.numGuests ||= 10;
 
-// nullish assignment operator (null or undefined)
-rest1.numGuests ??= 10; // 0 will not short circuit, just undefined and null
-rest2.numGuests ??= 10;
+// // nullish assignment operator (null or undefined)
+// rest1.numGuests ??= 10; // 0 will not short circuit, just undefined and null
+// rest2.numGuests ??= 10;
 
-// AND assignment operator
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
+// // AND assignment operator
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
+// // rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
+
+//////////////////////////////////////////
+
+// CODING CHALLENGE 1
+
+//////////////////////////////////////////
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// // 1.
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
+
+// // 2.
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+
+// // 3.
+
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// // 4.
+// const players1Final = [...players1, 'Thiggo', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+
+// // 5.
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
+
+// //6.
+// const printGoals = function (...players) {
+//   for (let i = 0; i < players.length; i++) {
+//     console.log(players[i]);
+//   }
+//   console.log(`${players.length} goal(s) were scored.`);
+// };
+
+// // TEST
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
+
+// // 7.
+
+// team1 < team2 && console.log(team1, 'Team 1 is the Likely winner');
+// team1 > team2 && console.log(team2, 'Team 2 is the Likely winner');
+
+//////////////////////////////////////////
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// Old way, normal for loop
+// for (let i = 0; i < menu.length; i++) {
+//   console.log(menu[i]);
+// }
+
+// new way, for of loop - gives current element
+for (const item of menu) console.log(item);
+
+// to get index
+// for (const item of menu.entries()) {
+//   console.log(`${item[0] + 1}: ${item[1]}`);
+// }
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i}: ${el}`);
+}
+
+console.log([...menu.entries()]);
