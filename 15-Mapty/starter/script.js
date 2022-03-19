4e2342aa1f2ea2da74a540cb4609527c09d48129
Faraz Chand <54415642+FarazChand@users.noreply.github.com>
2022-03-19 10:40:09 -0400

    General Update
    - Create the 'Workout' Parent Class and its two child classes to store user input data
    
    - implemented a way to create Workouts and make their marker appear on the map
    
    - other general features added

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -5,6 +5,64 @@
 // Variable Declarations:
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+// Creating Workout Parent Class to store user input data:
+class Workout {
+  // Getting current date:
+  date = new Date();
+  // Creating a unique ID:
+  // - turns the current date into a string
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    // Storing common data from user input into object's properties:
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+// Creating Running Child Class:
+class Running extends Workout {
+  type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  //Child class specific methods for running:
+  calcPace() {
+    // in min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  //Child class specific methods for running:
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / this.duration / 60;
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+
+// console.log(run1);
+// console.log(cycling1);
+
+///////////////////////////////
+// APPLICATION ARCHITECTURE
+//////////////////////////////
+
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -18,6 +76,7 @@ class App {
   // Creating Private Instance Properties of the App Class
   #map;
   #mapEvent;
+  #workouts = [];
   constructor() {
     // Getting the Location of the User:
     this._getPosition();
@@ -64,14 +123,7 @@ class App {
         '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
-    // // Creating and adding initial marker on current position:
-    // L.marker(coords)
-    //   .addTo(this.#map)
-    //   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    //   .openPopup();
-
     // Adding the leaflet event handler to the map object, Show Form:
-    // - handling clicks on the map
     this.#map.on('click', this._showForm.bind(this));
   }
 
@@ -85,23 +137,74 @@ class App {
   }
 
   _toggleElevationField() {
-    // 9. Selecting which Elements should toggle the 'hidden' class:
+    // Selecting which Elements should toggle the 'hidden' class:
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
 
   _newWorkout(e) {
+    // Creating a function to validate data
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
     // Prevent the Default behavior of reloading the page:
     e.preventDefault();
-    // Clear input fields
+
+    // Get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value; //Default as string, changed to num
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    // If workout running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      // Check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers! ');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    // If workout cycling, creat cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+
+      // Check if data is valid
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers! ');
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    // Add new object to workout array
+    this.#workouts.push(workout);
+    console.log(workout);
+
+    // Render workout on map as marker
+    this.renderWorkoutMarker(workout);
+    // Render workout on list
+
+    // Hide form + Clear input fields
     inputDistance.value =
       inputDuration.value =
       inputCadence.value =
       inputElevation.value =
         '';
-    // Display marker:
-    const { lat, lng } = this.#mapEvent.latlng;
-    L.marker([lat, lng])
+  }
+
+  renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         // Setting pop up Properties:
@@ -110,10 +213,10 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('Working')
+      .setPopupContent(workout.distance.toString())
       .openPopup();
   }
 }
@@ -532,3 +635,13 @@ if (navigator.geolocation) {
 // - in our html, the form has the class of "form" and "hidden", so we will be using DOM manipulation to add/remove the hidden class
 
 //
+
+// The 'this' keyword in our instantiation of the child data classes:
+// - just a quick walk-through on how the this keyword is assigned in this process, using the Running class as an example
+// - first we create a new instance of the 'Running' class
+// - this creates an empty object, which the 'this' keyword of our 'Running' class's constructor will point to
+// - the instance will also link its prototype with the 'Running' class's prototype and return the object to the variable we want to store it in
+// - note that the 'Running' child class's prototype is linked to the 'Workout' Parent class's prototype already
+// - now the constructor function is called immediately, which is followed by the immediate execution of the 'super' function
+// - this super function calls the 'Workout' class's constructor, passing the relevant parameters and also changing its 'this' key word to the instance
+// - this means that the methods that are called in the workout parents constructor will now point to the instance as well
