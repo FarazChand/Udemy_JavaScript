c58e15e21a593585c31242560496dd1d0e327bd3
Faraz Chand <faraz.chand@hotmail.com>
2022-03-21 06:40:17 -0400

    - General Update
    - Starting to add some more features to the already finished Mapty app, I think I should make a new folder specifically for it on GitHub.. just have to figure out how to do that

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -263,6 +263,7 @@ class App {
     let html = `
       <li class="workout workout--${workout.type}" data-id="${workout.id}">
         <h2 class="workout__title">${workout.description}</h2>
+        <div class="list-btns">Select</div>
         <div class="workout__details">
           <span class="workout__icon">${
             workout.type === 'running' ? '🏃‍♂️' : '🚴‍♂️'
