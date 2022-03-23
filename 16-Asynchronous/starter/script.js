823b9effd6a2889152d3372c0e9166bbedaaf05f
Faraz Chand <54415642+FarazChand@users.noreply.github.com>
2022-03-23 17:46:48 -0400

    General Uppdate

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -16,6 +16,7 @@ const getCountryData = function (country) {
   request.send();
   // Using an event listener to listen for the load event emitted by the Send request:
   request.addEventListener('load', function () {
+    // Data that is sent back:
     console.log(this.responseText);
     // Turning JSON into an OBJECT:
     const [data] = JSON.parse(this.responseText);
@@ -50,7 +51,7 @@ const getCountryData = function (country) {
 
 // These AJAX calls all happen at the same time:
 // - these AJAX calls are all fired immediately
-// - note that these AJAX calls all get fired way before the AJAX calls are complete
+// - note that these AJAX calls all finish firing way before any of the AJAX calls are complete
 // - requests may not arrive at the same time, might fire one before the other
 // - this means that returning data actually comes back in a random order, making it sometimes appear in a different order on the page
 // - if we wanted a predefined order, we would have to chain the requests
@@ -71,18 +72,20 @@ getCountryData('canada');
 
 // Sending request, fetches data in the background:
 // - after we send our "GET" request, we now have to request a "SEND" so that we can receive the data - once finished, a load event is emitted
-// - this process takes long and runs in the background, so we can't just store the result in a variable, because that line of code would execute before the request even finished
+// - this process takes long and runs in the background, so we can't just store the result in a variable, because that line of code would execute before the request even finished - making the value stored undefined?
 
 // Using an event listener to listen for the load event emitted by the Send request:
 // - instead, we listen for the load event and run a callback function once it happens
 // - we are sent back a JSON string which is saved into one of the request object's properties
 // - we can look at the data by accessing the request objects '.responseText' property
 // - what we get in return is a JSON formatted string containing the information we requested
-// - we just have to turn this result into an object now|
+// - we just have to turn this result into an object now
 
 // Turning JSON into an OBJECT:
 // - we do this by passing the JSON string into the 'JSON.parse' method
-// - in this case, this returns an array of one index containing the new object - this is because of how the JSON was formatted, we can simply destructure this
+// - in this case, this returns an array of one index containing the new object
+// - this is because of how the JSON was formatted, we can simply destructure this
+// - by doing so, we will be dealing with a single obeject with all the data sent back from the request
 
 //////////////////////////////////////////////////
 
@@ -102,7 +105,7 @@ getCountryData('canada');
 
 // Asynchronous:
 // - Asynchronous code is executed after a task that runs in the 'background' finishes, e.g. a timer with a callback
-// - asynchronous code in non- blocking
+// - asynchronous code is non- blocking
 // - Execution does not wait for an asynchronous task to finish its work, so the rest of the code can keep running normally
 // - callback functions ALONE do not make code asynchronous, e.g. the .map method for arrays
 // - event listeners ALONE do not make code asynchronous..
@@ -144,3 +147,109 @@ getCountryData('canada');
 // https://restcountries.com/v3.1/
 
 //////////////////////////////////////////////////
+
+// How the Web Works: Requests and Responses:
+// - whenever we try to access a web server, the browser - which is the client, sends a request to the server
+// - the server will then send  back a response
+// - this response contains the data or the web page that we requested
+// - this process works the exact same way, no matter if we are accessing an entire webpage, or just some data from a web API
+// - this whole process is called the "Reequest-response model" or the "Client-server architecture"
+
+// URLs:
+// Example: https://restcountries.com/v3.1/name/${country}
+
+// - Every URL gets an 'http' or an 'https', which is for the protocol that will be used on this connection
+// then, we have the 'domain name', which is 'restcountries.com' in this case
+// then also, after a backslash, we have the so called 'resource' that we want to access, which is '/v3.1/name/${country}' in this case
+
+// - now, the shown domain name in the Example url is actually NOT real the address of the server
+// - it's really just a nice name that is easy for us to memorize
+// - what this means is that we a need a way of converting the domain name to the real address of the server
+// - this happens through a so called 'DNS', which stands for 'Domain Name Server'
+// - DNS are a special kind of server - they are basically like the phone books of the internet
+
+// - The first step that happens when we access any web server:
+// - the browser makes a request to a DNS
+// - this special server will then simply match the web address of the url to the servers REAL IP address
+// - this all happens through your internet service provider
+// - after the IP address has been sent back to the browser, we can finally call it
+
+// IP:
+// Example: https://104.17.142.889.443
+// - this is what the real address looks like
+// - it still has the protocol in the beginning (http/https)
+// - then comes the IP address
+// - the last numbers are the port that we access on the server (443)
+
+// - this port number is really just to identify a specific service that is running on a server
+// - so you can think of it like a sub address
+// - the default '443' for HTTPS, 80 for HTTP
+// - the port number has nothing to do with the resource part of the URL that we want to access
+
+// - one we have the real IP address, a TCP (TCP/IP ?) socket connection is established between the browser and the server - meaning they are now connected
+// - this connection is typically kept alive for the entire time that it takes to transfer all files of the website or all data
+
+// TCP: the transmission control protocol
+// IP: the internet protocol
+// - together, they are communication protocols that define exactly how data travels accross the web
+// - they are basically the internets fundamental control system
+// - this is because they are the ones that set the rules on how data moves on the internet
+
+// - now its time to finaly make our request
+// - the request we make is a http protocol
+// - http stands for 'hyper text transfer protocol'
+// - after tcp/ip, http is another communication protocol
+// - a communication protocol is simply a system of rules that allows two or more parties to communicate
+// - in the case of 'http', it's just a protocol that allows that allows clients and webservers to communicate
+// - this works by sending request and response messages from clients to server an back
+
+// LOOK AT VIDEO TO SEE EXAMPLE OF AN HTTP REQUEST
+// - the beginning of an http request is called a 'start line', which contains the http method that is used in the request, then the request target, and alos the http version
+// - there are many http methods available - but the most important ones are GET, POST, PUT and PATCH
+// - GET simply requests data
+// - POST sends data
+// - PUT and PATCH basically modifies data
+
+// - the request target is where the server is told that we want to access the specified resource (from the URL)
+// - if the target is empty, we would just be accessing the websites root
+
+// - the next part of the request are the request headers (many different possiblities)
+// - which is just some information that we send about the request itself
+// - there are tons of standered different headers
+
+// - in the case that we are sending data to a server, there will also be a request body
+// - this body will contain the data that we are sending, for example, coming from an HTML form
+
+// - Now of course, it's not us developer who manually write these http requests
+// - it's still helpful and valuable to understand what an http request and also a response look like
+
+// - the main difference between http, and https is that https is encrypted using TLS or SSL - which are yet some more protocols
+// - besides that, the logic behind http requests and responses still apply to https
+
+// - so once our request is formed, it then hits the server - which will then be working on it until it has our data or webpage ready to send back
+// - once it's ready, it will send it back using an http response
+// - the http response message actually looks quite similar to the request (video)
+// - it also contains a startline, headers and a body
+// - the startline of a response message has a version, a status code and a status message
+// - these are used to let the client know whether the request has been successful or failed, e.g. 200 means OK... or 404 Page not found
+// - the response headers are information about the response itself, many possiblities, in fact, we can actually make up our own
+// - the last part of the response is again the body, which is present in most responses
+// - this body usually contains the JSON data coming back from an API or the HTML of the webpage we requested.. or something similar
+
+// - an API only requires one request, a website might require many
+// - this is because for a website, the first request will return an HTML address, which contains the information on all the assets that it needs
+// - these assets being: JavaScript files, CSS files, Images, and other assets
+// - this information is scanned by the browser
+// - for each different file, there will be a new http request made to the server
+// - there can be multiple requests happening at the same time, but the amount is still limited - otherwise the connection would start to slow down
+// - when all the files have finaly arrived, then the webpage can be rendered in the browser according to the HTML, CSS JavaScript Specifications that we already know
+
+// TCP/IP:
+// - the communtication protocols that define how data travels accross the web
+// - the job of TCP is to break the requests and responses down into thousands of small chunks called 'packets' before they are sent
+// - once these small packets arrive at their final destination, TCP will reassemble all the packets into the original request or response
+// - this is necessary so that each packet can take a different route through the internet
+// - this way, the message arrives at the destination as quick as possible, which would not be possible if we sent the entire data simply as a big chunk
+
+// - the job of the IP protocol is to actually send and route these packets through the internet
+// - it ensures that they arrive at the destination they should go by using IP addresses on each packet
