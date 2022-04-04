625c53ad158f496557b1385b9592bcf6fb42e2c0
Faraz Chand <54415642+FarazChand@users.noreply.github.com>
2022-04-04 08:27:23 -0400

    GC

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,21 +1,59 @@
-// Importing module:
+//////////////////////////////////////////////
+// Exporting and importing in ES6 Modlules: //
+//////////////////////////////////////////////
+
+console.log(`Importing module`);
+
+// Importing module, using named exports:
 // - can rename variables to desired identifier while importing
 
 /*
 import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 addToCart('bread', 5);
 console.log(price, tq);
-*/
-
-console.log(`Importing module`);
 // console.log(shippingCost); // Cannot do this, variable is private to it's module
+*/
 
 // Importing everything from a module
 // - works kind of like a class/object (camel case convention)
 // - gives access to all exports of the specified module
+
+/*
 import * as ShoppingCart from './shoppingCart.js';
 ShoppingCart.addToCart('bread', 5);
 console.log(ShoppingCart.totalPrice);
+*/
+
+// Importing default export:
+// - we can name the default import what we want, named it add in this case
+// - should never import default and named exports together, we will during this example though (just best practice not to)
+
+// /*
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+// Live connection between modules, not a copy but a pointer:
+console.log(cart);
+
+////////////////////////////////
+// Top-level Await (ES2022): //
+///////////////////////////////
+
+// - in ES2022 we can now 'await' on the top level within modules
+// - note that this only works within modules
+// - also note that this blocks execution until the promise is settled
+// - also note that the main script will wait for any blocking code from any of it's dependencies to execute before it starts executing it's own code
+// - top level await is very useful, but we should be careful when using it because of the fact that it blocks code execution
+
+console.log('Start fetching');
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log('Something');
+
+// */
+
 ////////////////////////////////
 
 // Notes:
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,8 +1,8 @@
-// Exporting module
 console.log('Exporting module');
+// Exporting module:
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 // Exporting a whole function:
 export const addToCart = function (product, quantity) {
@@ -14,5 +14,14 @@ export const addToCart = function (product, quantity) {
 const totalPrice = 300;
 const totalQuantity = 34;
 
+// Named exports:
 // - can rename variables to desired identifier while exporting
 export { totalPrice, totalQuantity as tq };
+
+// Default export:
+// - can only be one
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} addedd to cart`);
+  console.log(cart);
+}
