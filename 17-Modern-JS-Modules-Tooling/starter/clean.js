bdb1ba0541da3ecbc19cc5e8c6079b01f7524afe
Faraz Chand <54415642+FarazChand@users.noreply.github.com>
2022-04-07 21:12:35 -0400

    GU
    - added git ignore file so I can ignore the node_modules folder... hope this works
    
    -almost done the JS course!!!! On to the last project

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,47 +1,75 @@
-const budget = [
+'strict mode';
+
+// const { spread } = require('lodash'); ?????
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
-  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: -450, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
   { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
   { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+//Pure function :D
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : [...state];
 };
 
-const getLimit = user => spendingLimits?.[user] ?? 0;
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
 
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : { ...entry }
+  );
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+
+newBudget3[0].value = 100000;
+console.log(newBudget3);
+console.log(finalBudget);
+
+// Impure
+const logBigExpenses = function (state, bigLimit) {
+  const cleanState = [...state];
+
+  const bigExpenses = cleanState
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+
+  console.log(bigExpenses);
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
 
-const checkExpenses = function () {
-  for (const entry of budget)
-    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
-};
-checkExpenses();
-
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget)
-    output +=
-      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
-
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-
-console.log(budget);
-logBigExpenses(1000);
+logBigExpenses(finalBudget, 1000);
