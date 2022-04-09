e5965e36642dc4cd1d14367c6e9cb77e4f6c6272
Faraz Chand <faraz.chand@hotmail.com>
2022-04-09 00:49:51 -0400

    Forkify001
    - created the forkify folder
    - added the json package file and some dependencies
    - added the .gitignore file
    - played around with the forkify API

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,45 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// const showRecipe = async function()
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/get?rId=36453'
+      // 'https://forkify-api.herokuapp.com/api/get?rId=47746'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.error} (${res.status})`);
+
+    // console.log(data.recipe.recipe_id);
+
+    let { recipe } = data;
+    recipe = {
+      id: recipe.recipe_id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings || 'no info',
+      cookingTime: recipe.cooking_time || 'no info',
+      ingredients: recipe.ingredients,
+    };
+
+    //
+
+    // CONSOLE LOGS
+    console.log(res);
+    console.log(data);
+    console.log(recipe);
+
+    // Catch Errors
+  } catch (err) {
+    alert(err);
+    console.error(err);
+  }
+};
+
+showRecipe();
