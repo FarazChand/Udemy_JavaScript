d86e3200f536c01006626881be0d54defb6b9165
Faraz Chand <faraz.chand@hotmail.com>
2022-04-15 00:01:27 -0400

    General Update

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -12,7 +12,7 @@ import 'regenerator-runtime/runtime';
 // }
 /////////////////////////////////////////////////
 
-console.log('Live server test');
+// console.log('Live server test');
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -23,6 +23,9 @@ const controlRecipes = async function () {
 
     if (!id) return;
     recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
 
     // 1) Loading recipe:
     await model.loadRecipe(id);
@@ -74,7 +77,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
